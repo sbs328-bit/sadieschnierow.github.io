ffff4b71c92978ce2d8836a099217d59311165a2
Sadie Schnierow <sbs328@cornell.edu>
2026-06-15 04:40:15 +0000

    Social media images

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,8 +3,8 @@ export default function Footer() {
     <footer className="site-footer">
       <div>
         <div style={{ display: "flex", flexDirection: "row", gap: "10px", justifyContent: "center" }}>
-        <a href="https://www.instagram.com/sadiebeacakes/" ><img style={{ width: "20px" }} src="/images/insta.png" alt="Instagram" /></a>
-        <a href="https://www.linkedin.com/in/sadie-schnierow/" ><img style={{ width: "20px" }} src="/images/linkedin.png" alt="LinkedIn" /></a>
+        <a href="https://www.instagram.com/sadiebeacakes/" ><img style={{ width: "20px" }} src="images/insta.png" alt="Instagram" /></a>
+        <a href="https://www.linkedin.com/in/sadie-schnierow/" ><img style={{ width: "20px" }} src="images/linkedin.png" alt="LinkedIn" /></a>
         </div>
         <p>sbs328@cornell.edu</p>
         <p>© 2026 Sadie Schnierow</p>
